29b2fb37f43cc64938b47840567d85f8fec143e4
alexandermaxwellogle <maxogle@gmail.com>
2022-02-10 14:51:01 -0500

    commit tree bc3a36fd163e180fcb76b0204c3b19bea4af9e30

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+      cats.length = 0;
+
+      cats.push('Milo', 'Otis', 'Garfield');
+    })
+
+    function destructivelyAppendCat(name) {
+        cats.push('Ralph')
+    }
+
+    function destructivelyPrependCat(name) {
+        cats.unshift('Bob')
+    }
+
+    function destructivelyRemoveLastCat(name) {
+        cats.pop()
+    }
+
+    function destructivelyRemoveFirstCat(name) {
+        cats.shift()
+    }
+    
+    
+
+    function appendCat(name) {
+        let newcats = [...cats, 'Broom']
+        return newcats;
+        }
+    
+    function prependCat(name) {
+        let newerCats = ['Arnold', ...cats]
+        return newerCats;
+    }
+
+    function removeLastCat(name) {
+        return cats.slice(0, -1); 
+    }
+
+    function removeFirstCat(name) {
+        return cats.slice(1);
+    }
+
+
+
+     
